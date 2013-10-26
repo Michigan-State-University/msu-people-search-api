75b2b26aaebee7d3042e83d645d6b843cdc2b533
Troy Murray <troy.murray@gmail.com>
2013-10-26 14:05:35 -0400

    Add basic route /api/1/users

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var express = require('express'),
 //Some Server configuration
 app.use(express.bodyParser());
 
+app.get('/', function(req, res) {
+  var text = "Hello World!";
+  res.send(text);
+});
+
+app.get('/api/1/users', function(req, res) {
+  var text = "Hello World!";
+  res.send(text);
+});
+
 
 //Start the app
 app.listen(8080);
